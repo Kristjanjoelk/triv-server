2eb9fbabbad825532c2d61ae49b96fbd81a22e4a
Kristjanjoelk <kristjanjoel@gmail.com>
2018-03-19 16:20:10 +0000

    server msg

--- index.js
+++ index.js
@@ -10,10 +10,16 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => {
-      console.log('Client disconnected')
-  });
+    console.log('Client connected');
+    socket.on('message', (msg) => {
+        console.log('Client says', msg);
+        io.send('message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('Client disconnected')
+    });
 });
+
+
 
 
